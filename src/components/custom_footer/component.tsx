8874e60071c5d9a987a4400fd0a971a8e04af1dd
Aman Verma <verma.aman0312@gmail.com>
2025-06-01 21:46:22 +0530

    update code....

---
+++ src/components/custom_footer/component.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CustomFooter = () => {
+  return (
+    <div className="h-[15rem] bg-background-secondary bg-opacity-30 rounded-t-3xl flex flex-col items-center justify-center gap-8 relative">
+      Footer
+    </div>
+  );
+};
+
+export default CustomFooter;
--- src/layouts/unauthenticated_layouts/home_layout/layout.tsx
+++ src/layouts/unauthenticated_layouts/home_layout/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeLayoutTemplate from "../../../templates/unauthenticated_templates/home_template/template";
+import CustomFooter from "../../../components/custom_footer/component";
 
 const HomePageLayout = () => {
   return (
     <div className="w-full">
       <HomeLayoutTemplate />
+      <CustomFooter />
     </div>
   );
 };
